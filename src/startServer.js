e63eb69da064dfc143716469170e766b2b915a73
Bruno Lopes <eusoubruno7@gmail.com>
2021-11-23 14:44:28 -0300

    feat: starting serevr

--- src/startServer.js
+++ src/startServer.js
@@ -1,1 +1,10 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from 'apollo-server';
+
+function startServer({ typeDefs, resolvers }) {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  server.listen().then(({ url }) => {
+    console.log(`✅ server started at ${url}`);
+  });
+}
+
+export default startServer;
