3adac579f9763ee8d943632c0f0441528e96212c
Bruno Lopes <eusoubruno7@gmail.com>
2021-11-25 10:35:31 -0300

    feat: creating merge typeDefs and resolvers

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,1 +1,10 @@
-export default {};
+import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
+import path from 'path';
+
+const reesolversArray = fileLoader(
+  path.join(__dirname, 'modules', '**', '*.js'),
+);
+
+const resolvers = mergeResolvers(reesolversArray);
+
+export default resolvers;
